efa3ad251b148ec958b771abe713bcb57ebac416
Sławek Kabik <slawek@thecapitals.nl>
2017-10-17 22:31:51 +0200

    refactoring

--- settings/comments.go
+++ settings/comments.go
@@ -5,11 +5,11 @@ import "strings"
 type Comments struct {}
 
 // checks if given string is a comment - starts with: /, //, #.
-func (comments *Comments) isComment(text string) bool {
+func (comments *Comments) isComment(configLine string) bool {
 	commentsPattern := []string{"/", "//", "#"}
 
-	for _, element := range commentsPattern {
-		if strings.HasPrefix(text, element) == true {
+	for _, comment := range commentsPattern {
+		if strings.HasPrefix(configLine, comment) == true {
 			return true
 		}
 	}
--- settings/validation.go
+++ settings/validation.go
@@ -4,7 +4,9 @@ type Validation struct {}
 
 // checks if current key and value are valid settings - cannot be empty strings.
 func (validation *Validation) isValid(key string, value string) bool {
-	if key == "" || value == "" {
+	const EMPTY = ""
+
+	if key == EMPTY || value == EMPTY {
 		return false
 	}
 	return true
